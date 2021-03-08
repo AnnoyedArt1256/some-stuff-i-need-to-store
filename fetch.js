28a466358c86d5e0d3332ae1075e846b7804675c
AnnoyedArt1256 <66080161+AnnoyedArt1256@users.noreply.github.com>
2021-03-08 17:59:37 +0300

    Create fetch.js

---
+++ fetch.js
@@ -0,0 +1,25 @@
+class Javascript {
+    getInfo() {
+        return {
+            "id": "Javascript",
+            "name": "Javascript",
+            "blocks": [{
+                    "opcode": "eval",
+                    "blockType": "reporter",
+                    "text": "eval [string]",
+                    "arguments": {
+                        "num1": {
+                        "string": {
+                            "type": "string",
+                            "defaultValue": 'alert("Hello World!")'
+                        }
+                    }
+                },
+            }],
+        "menus": { //we will get back to this in a later tutorial
+        }
+    };
+    eval({string}) {
+        return eval(string)
+    };
+}
